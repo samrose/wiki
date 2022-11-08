6fb4a01e62b4b38ae7020932233ddac1ad1296d5
Sam Rose <sam@floxdev.com>
2022-11-08 11:29:19 -0500

    project based

--- flake.nix
+++ flake.nix
@@ -1,19 +1,16 @@
 {
   inputs = {
     dream2nix.url = "github:nix-community/dream2nix";
-    src.url = "https://registry.npmjs.org/wiki/-/wiki-0.28.0.tgz";
-    src.flake = false;
   };
 
   outputs = {
     self,
     dream2nix,
-    src,
   } @ inp:
     (dream2nix.lib.makeFlakeOutputs {
       systems = ["x86_64-linux"];
       config.projectRoot = ./.;
-      source = src;
+      source = ./.;
       settings = [
         {
           subsystemInfo.noDev = true;
